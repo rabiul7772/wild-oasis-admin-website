186eb4ed88542b96d620fbe50de5266c1f2c7e69
rabiul7772 <rabiulakand777@gmail.com>
2026-01-09 16:12:45 +0600

    feat: dashboard statistics, stay duration chart and sales chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { useRecentBookings } from './useRecentBookings';
+import { useRecentStays } from './useRecentStays';
+import Spinner from '../../ui/Spinner';
+import Stats from './Stats';
+import useCabins from '../cabins/useCabins';
+import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +13,33 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+const DashboardLayout = () => {
+  const { isPending, bookings } = useRecentBookings();
+  const {
+    isPending: staysPending,
+    stays,
+    confirmedStays,
+    numDays
+  } = useRecentStays();
+
+  const { cabins, isPending: cabinsPending } = useCabins();
+
+  if (isPending || staysPending || cabinsPending) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>activity</div>
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  );
+};
+
+export default DashboardLayout;
